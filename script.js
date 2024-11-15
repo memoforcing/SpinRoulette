994e7bcbd82dc5f6ae2547903fd7dcbf0df1982f
Isaac <memoforcing@gmail.com>
2024-11-15 05:41:20 -0300

    Casiono Roulette 01

---
+++ script.js
@@ -0,0 +1,15 @@
+let container = document.querySelector(".container");
+let btn = document.getElementById("spin");
+let number = Math.ceil(Math.random() * 1000);
+
+let clicks = 0;
+btn.onclick = function () {
+clicks += 1;
+ if(clicks <= 5 ){
+	container.style.transform = "rotate(" + number + "deg)";
+	number += Math.ceil(Math.random() * 1000);
+    if(clicks == 5){
+        clicks = 0;
+    }
+  }
+}
